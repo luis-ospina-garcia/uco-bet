100ecabcafb6c47ff798174c560fc34d3078107d
luis-ospina-garcia <luisospi1541@gmail.com>
2024-11-08 18:15:27 -0500

    correciones

--- src/main/java/co/edu/uco/ucobet/generales/infrastructure/primaryadapter/services/HtmlSanitizerExample.java
+++ src/main/java/co/edu/uco/ucobet/generales/infrastructure/primaryadapter/services/HtmlSanitizerExample.java
@@ -2,12 +2,16 @@ package co.edu.uco.ucobet.generales.infrastructure.primaryadapter.services;
 
 import org.owasp.html.Sanitizers;
 import org.owasp.html.PolicyFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HtmlSanitizerExample {
+    private static final Logger logger = LoggerFactory.getLogger(HtmlSanitizerExample.class);
+
     public static void main(String[] args) {
         String untrustedHTML = "<a href='http://example.com'>Click here</a><script>alert('XSS')</script>";
         PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
         String safeHTML = policy.sanitize(untrustedHTML);
-        System.out.println(safeHTML);
+        logger.info("Sanitized HTML: {}", safeHTML);
     }
 }
